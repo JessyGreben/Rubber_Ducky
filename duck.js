7de31c5fa91d35d73286de00ac7b93c40a02d89b
Jessica Grebenschikov <jessica.greben1@gmail.com>
2015-01-30 11:23:23 -0800

    add altino duck img and next button

--- duck.js
+++ duck.js
@@ -1,17 +1,33 @@
 $( document ).ready(function() {
     console.log( "ready!" );
-$(".slide-bar").hide();
+	$(".slide-bar-personality").hide();
+	$(".slide-bar-clothes").hide();
+	$(".slide-bar-accessories").hide();
 
-$("#button").click(function() {
-  $(".slide-bar").toggle( 'explode', 500 );
-});
+	$("#button").click(function() {
+  	$(".slide-bar-personality").toggle();
+	});
 
+//when you click 'Latino' button on Personality list, the classic duck img src changes to latino_duck img
+	//why does css change, when only changing the src
+  $("#latino").click(function () {
+    $('#classic_duck').attr('src', 'imgs/latino_duck.png');
+  });
 
-// 'blind'
-$('input[name=personality]').click(function(){
-  console.log($('input[name=personality]:checked').val());
-})
+//click next button change slide-bar to clothing slide-bar
+	$('#next').click(function () {
+		$(".slide-bar-clothes").toggle();	
+	});
 
+	$('#next_two').click(function () {
+		$(".slide-bar-accessories").toggle();	
+	});
+            
+      
+//'blind'
+// $('input[name=personality]').click(function(){
+//   console.log($('input[name=personality]:checked').val());
+// })
 
 
 });
